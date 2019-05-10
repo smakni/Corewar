1496ca6b14569a5a502e11fccc5e837a338fed80
Jeremie <jeremiegthr@gmail.com>
2019-05-10 23:16:32 +0200

    sti encoding

--- includes/asm.h
+++ includes/asm.h
@@ -2,5 +2,10 @@
 # define ASM_H
 
 # include "../libft/libft.h"
+# include "../ressources/op.h"
+
+# define IDENTICAL	0
+
+int		ft_strccmp(const char *s1, const char *s2, const char limit);
 
 #endif
