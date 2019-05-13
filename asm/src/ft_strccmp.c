7ce863764924e05072306ed885b527cbc4b61654
smakni <sabrimakni@gmail.com>
2019-05-13 15:51:17 +0200

    modif ft_strccmp

--- asm/src/ft_strccmp.c
+++ asm/src/ft_strccmp.c
@@ -1,15 +1,28 @@
 #include "../../includes/asm.h"
 
-int		ft_strccmp(const char *s1, const char *s2, const char limit)
+int		ft_strccmp(const char *s1, const char *s2)
 {
-	while (*s1 && *s2 && *s1 == *s2 && *s2 != limit)
+	while (*s1 && *s2 && *s1 == *s2 && *s2 != ' ' && *s2 != '\t')
+	{
+		if (*s1 != *s2)
+			return (-1);
+		s1++;
+		s2++;
+	}
+	return (0);
+}
+
+/*
+int		ft_strccmp(const char *s1, const char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2 && *s2 != ' ' && *s2 != '\t')
 	{
 		s1++;
 		s2++;
 	}
-	if (*s1 == '\0' && *s2 == ' ')
+	if (*s1 == '\0' && (*s2 == ' ' || *s2 == '\t'))
 		return ((unsigned char)*s1 + 32 - (unsigned char)*s2);
-	if (*s2 == '\0' && *s1 == ' ')
+	if (*s2 == '\0' && (*s1 == ' ' || *s1 == '\t'))
 		return ((unsigned char)*s1 - (unsigned char)*s2 + 32);
 	return ((unsigned char)*s1 - (unsigned char)*s2);
-}
+}*/
