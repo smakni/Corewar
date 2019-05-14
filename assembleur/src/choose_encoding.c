061dc4cabd85becbbeca5fbba835230a871f3590
Deveul <vrenaudi@student.42.fr>
2019-05-14 16:03:58 +0200

    activated my encoding functions

--- assembleur/src/choose_encoding.c
+++ assembleur/src/choose_encoding.c
@@ -18,12 +18,20 @@ int		choose_encoding(t_parser *data, int i)
 		return (ft_encode_ldi(data));
 	if (ft_strccmp("fork", &data->line[i]) == IDENTICAL)
 		return (ft_encode_fork(data));
+	if (ft_strccmp("lfork", &data->line[i]) == IDENTICAL)
+		return (ft_encode_lfork(data));
 	if (ft_strccmp("live", &data->line[i]) == IDENTICAL)
 		return (ft_encode_live(data));
 	if (ft_strccmp("ld", &data->line[i]) == IDENTICAL)
 		return (ft_encode_ld(data));
+	if (ft_strccmp("lld", &data->line[i]) == IDENTICAL)
+		return (ft_encode_lld(data));
+	if (ft_strccmp("lldi", &data->line[i]) == IDENTICAL)
+		return (ft_encode_lldi(data));
 	if (ft_strccmp("st", &data->line[i]) == IDENTICAL)
 		return (ft_encode_st(data));
+	if (ft_strccmp("aff", &data->line[i]) == IDENTICAL)
+		return (ft_encode_aff(data));
 	choose_encoding_suite(data, i);
 	return (0);
 }
