bfa91e890eb350a9d6aabf6790d827600cb2d93e
Sabri MAKNI <sabrimakni@gmail.com>
2019-05-20 12:29:16 +0200

    add exec_op

--- vm/src/read_memory.c
+++ vm/src/read_memory.c
@@ -3,62 +3,91 @@
 /*                                                        :::      ::::::::   */
 /*   read_memory.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sabri <sabri@student.42.fr>                +#+  +:+       +#+        */
+/*   By: smakni <smakni@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 00:29:55 by sabri             #+#    #+#             */
-/*   Updated: 2019/05/20 02:54:47 by sabri            ###   ########.fr       */
+/*   Updated: 2019/05/20 12:28:45 by smakni           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/vm.h"
 
+//for each champ
+//check if cicles to wait = 0
+//if = 0
+//read memory from pc
+//save op_len
+//exec op >> check if op exec at cycle = 0 or first cycle
+//exec op
+//check if carry change
+//pc += op_len
+
+int		exec_op(t_env *env, int j)
+{
+	if (env->memory[env->champ[j].pc] == 0x01)
+		ft_printf("live\n");
+	else if (env->memory[env->champ[j].pc] == 0x02)
+		ft_printf("ld\n");
+	else if (env->memory[env->champ[j].pc] == 0x03)
+		ft_printf("st\n");
+	else if (env->memory[env->champ[j].pc] == 0x04)
+		ft_printf("add\n");
+	else if (env->memory[env->champ[j].pc] == 0x05)
+		ft_printf("sub\n");
+	else if (env->memory[env->champ[j].pc] == 0x06)
+		ft_printf("and\n");
+	else if (env->memory[env->champ[j].pc] == 0x07)
+		ft_printf("or\n");
+	else if (env->memory[env->champ[j].pc] == 0x08)
+		ft_printf("xor\n");
+	else if (env->memory[env->champ[j].pc] == 0x09)
+		ft_printf("zjump\n");
+	else if (env->memory[env->champ[j].pc] == 0x0a)
+		ft_printf("ldi\n");
+	else if (env->memory[env->champ[j].pc] == 0x0b)
+		ft_printf("sti\n");
+	else if (env->memory[env->champ[j].pc] == 0x0c)
+		ft_printf("fork\n");
+	else if (env->memory[env->champ[j].pc] == 0x0d)
+		ft_printf("lld\n");
+	else if (env->memory[env->champ[j].pc] == 0x0e)
+		ft_printf("lldi\n");
+	else if (env->memory[env->champ[j].pc] == 0x0f)
+		ft_printf("lfork\n");
+	else if (env->memory[env->champ[j].pc] == 0x10)
+		ft_printf("aff\n");
+	return (1);
+}
+
 int		read_memory(t_env *env)
 {
-	int i;
-	int	j;
-	int rounds;
-	int	read;
-	int	op_len;
+	int 			i;
+	int				j;
+	int 			rounds;
+	int				op_len;
 
 	i = 0;
 	j = 0;
 	rounds = 1;
 	ft_printf("ROUND[%3d]\n", rounds);
-	while (rounds <= 10)
+	while (rounds <= 5)
 	{
-		//for each champ
-		//check if cicles to wait = 0
-		//if = 0
-		//read memory from pc
-		//save op_len
-		//exec op >> check if op exec at cycle = 0 or first cycle
-		//exec op
-		//check if carry change
-		//pc += op_len
 		j = 0;
 		while (j < env->nb_champs)
 		{
 			ft_printf(">>READ_CHAMP[%d]<<\n", j);
-			read = 0;	//check op len
 			if (env->champ[j].cycles == 0)
-			{
-				op_len = 10;
-				while(read < op_len) //check op len
-				{
-					ft_printf("read_champ[%d]>>[%.2x]\n", j, env->memory[env->champ[j].pc++]);
-					read++;
-				}
-			}
-			//env->champ[j].pc += 4; // += op_len
+				op_len = exec_op(env, j);
+			env->champ[j].pc += op_len;
 			usleep(500000);
 			j++;
 		}
-		if (i == 10) //CYCLE_TO_DIE
+		if (i == 100) //CYCLE_TO_DIE
 		{
 			rounds++;
+			i = 0;
 			ft_printf("ROUND[%3d]\n", rounds);
 			usleep(500000);
-			i = 0;
 		}
 		i++;
 	}
