02b3645d9135301c38f995663be30e4fd94bde47
jeremie-gauthier <jeremiegthr@gmail.com>
2019-05-31 16:42:25 +0200

    fix sti with indirect

--- vm/src/op_sti.c
+++ vm/src/op_sti.c
@@ -4,7 +4,8 @@ void	op_sti(t_env *env, unsigned j)
 {
 	unsigned	reg_content;
 	int			cursor;
-	short		dest;
+	int			v1;
+	int			dest;
 	int			k;
 	int			x;
 	int			y;
@@ -13,8 +14,14 @@ void	op_sti(t_env *env, unsigned j)
 	dest = env->champ[j].pc;
 	cursor = 1;
 	reg_content = (unsigned)get_value_index(env, j, &cursor, 1);
-	dest += get_value_index(env, j, &cursor, 2);
+	v1 = get_value_index(env, j, &cursor, 2);
+	if (type_param(env->memory[env->champ[j].pc + 1], 2) == IND_CODE)
+		dest += read_bytes(env, env->champ[j].pc + (v1 % IDX_MOD));
+	else
+		dest += get_value_index(env, j, &cursor, 2);
 	dest += get_value_index(env, j, &cursor, 3);
+	dest %= IDX_MOD;
+	// ft_printf("%i\n", dest);
 	env->memory[dest] = reg_content >> 24;
 	env->memory[dest + 1] = reg_content >> 16;
 	env->memory[dest + 2] = reg_content >> 8;
