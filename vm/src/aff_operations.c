0c0d8700e48bc16f391b46de29a335d941b39771
smakni <sabrimakni@gmail.com>
2019-06-04 15:38:43 +0200

    upd aff_oprations

--- vm/src/aff_operations.c
+++ vm/src/aff_operations.c
@@ -6,7 +6,7 @@
 /*   By: sabri <sabri@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 14:44:54 by sabri             #+#    #+#             */
-/*   Updated: 2019/06/04 14:45:35 by sabri            ###   ########.fr       */
+/*   Updated: 2019/06/04 15:37:53 by sabri            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,16 @@ void 	aff_operations(t_env *env, unsigned j, int save)
 	int i;
 
 	i = 0;
+	ft_printf("PROC[%d]>>CARRY[%d]", j, env->champ[j].carry);
 	if (env->memory[save] == 0x09 && env->champ[j].carry == 1)
-		ft_printf("PROC[%d]>>PC[%d]>>ZJMP>>>>[%d]\n", j, save
+		ft_printf("PC[%d]>>ZJMP>>>>[%d]\n", save
 						,read_bytes(env->memory, save + 1, 2));
 	else if (env->memory[save] == 0x0c)
-		ft_printf("PROC[%d]>>PC[%d]>>FORK>>>>[%d]\n", j, save
+		ft_printf(">>PC[%d]>>FORK>>>>[%d]\n", save
 						,read_bytes(env->memory, save + 1, 2));
 	else
 	{
-		ft_printf("PROC[%d]>>PC[%d]>>OP>>>>[ ", j, save);
+		ft_printf(">>PC[%d]>>OP>>>>[ ", save);
 		while (save + i < env->champ[j].pc)
 		{
 			ft_printf("%.2x ", env->memory[save + i]);
