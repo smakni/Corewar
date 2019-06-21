5aef2bd129acfb9cd3577d10099101cf85c93ed8
jeremie-gauthier <jeremiegthr@gmail.com>
2019-06-21 16:36:00 +0200

    add visu options and some protections

--- bonus_corewar/controller.py
+++ bonus_corewar/controller.py
@@ -40,10 +40,8 @@ class Controller:
 			try:
 				if not self.checklist["received"]:
 					self.receive_champ()
-					waiting = False
 				if not self.checklist["sent"]:
 					self.get_confirmation()
-					waiting = False
 			except KeyboardInterrupt:
 				print("\n[*] Disconnection requested.")
 				self.close_connections()
