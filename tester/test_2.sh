fba9310969b4774ddc2aa3f957f5eb5d32112bbb
smakni <sabrimakni@gmail.com>
2019-06-27 01:28:52 +0200

    updated script

--- tester/test_2.sh
+++ tester/test_2.sh
@@ -12,7 +12,58 @@ read -p "Do you need to search the champions paths ? (y/n) " answer
 
 if [ "$answer" = "y" ] || [ "$answer" = "Y" ] || [ "$answer" = "yes" ]
 then
-find .. -name "*.cor" 
+
+unset options i
+while IFS= read -r -d $'\0' f; do
+  options[i++]="$f"
+done < <(find .. -name "*.cor" -print0 )
+
+select champs in "${options[@]}" "quit"; do
+  case $champs in
+    *.cor)
+      echo "$champs selected"
+
+      	find .. $champs > /dev/null
+
+		if [ $? != 0 ] || [ -z $champs ]
+		then
+		echo -e "${RED}ERROR : Wrong champions path${NOCOLOR}"
+
+		else
+		echo "Store first to exec_1.debug"
+		../corewar $champs > exec_1.debug
+
+		echo "Store second to exec_2.debug"
+		../ressources/vm_champs/corewar -v 31 $champs > exec_2.debug
+
+		read -p "Select arguments for diff : " args
+		read -p "Do you need to store the output | less ? (y/n/less) " answer2
+
+		fi
+
+		if [ "$answer2" = "y" ] || [ "$answer2" = "Y" ] || [ "$answer2" = "yes" ]
+		then
+		echo "Store diff to diff.debug"
+		diff $args exec_1.debug exec_2.debug > diff.debug
+
+		elif [ "$answer2" = "less" ]
+		then
+		diff $args exec_1.debug exec_2.debug | less
+
+		else
+		diff $args exec_1.debug exec_2.debug
+
+		fi
+      ;;
+    "quit")
+      #echo "You chose to stop"
+      exit
+      ;;
+    *)
+      echo "This is not a number"
+      ;;
+  esac
+done
 
 fi
 
