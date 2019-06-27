6a75e84988ab132bd73477b5f6d2cfde7443de0c
Caroline MOULINIER <cmoulini@e3r12p6.42.fr>
2019-06-27 17:51:20 +0200

    norm

--- vm/src/op_add.c
+++ vm/src/op_add.c
@@ -3,62 +3,60 @@
 /*                                                        :::      ::::::::   */
 /*   op_add.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jergauth <jergauth@student.42.fr>          +#+  +:+       +#+        */
+/*   By: cmoulini <cmoulini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/04 22:57:09 by cmoulini          #+#    #+#             */
-/*   Updated: 2019/06/27 10:44:42 by jergauth         ###   ########.fr       */
+/*   Updated: 2019/06/27 17:47:51 by cmoulini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/vm.h"
 
+static void	set_reg(t_env *env, unsigned int j, int sum, int nb_reg)
+{
+	if (sum == 0)
+		env->process[j].carry = 1;
+	else
+		env->process[j].carry = 0;
+	env->process[j].op.name = "add";
+	env->process[j].r[nb_reg] = sum;
+}
+
+static void	add_op(t_env *env, unsigned int j, int nb_reg, int *sum)
+{
+	int reg_content;
+
+	reg_content = env->process[j].r[nb_reg];
+	if (env->verb == 1)
+		save_param(env, j, nb_reg, REG_CODE, 0);
+	if (nb_reg >= 1 && nb_reg <= REG_NUMBER)
+		*sum += reg_content;
+}
+
 void		op_add(t_env *env, unsigned int j)
 {
 	int cursor;
-	int	reg_content;
-	int	nb_reg1;
-	int	nb_reg2;
-	int	nb_reg3;
+	int nb_reg[3];
 	int	sum;
 
 	cursor = 1;
 	if (check_args(env, j, &cursor, 3))
 	{
-		cursor++;
 		sum = 0;
-		nb_reg1 = env->process[j].op.saved[cursor];
+		nb_reg[0] = env->process[j].op.saved[2];
+		add_op(env, j, nb_reg[0], &sum);
+		nb_reg[1] = env->process[j].op.saved[3];
+		add_op(env, j, nb_reg[1], &sum);
+		nb_reg[2] = env->process[j].op.saved[4];
 		if (env->verb == 1)
-			save_param(env, j, nb_reg1, REG_CODE, 0);
-		if (nb_reg1 >= 1 && nb_reg1 <= 16)
-		{
-			reg_content = env->process[j].r[nb_reg1];
-			sum += reg_content;
-		}
-		cursor++;
-		nb_reg2 = env->process[j].op.saved[cursor];
-		if (env->verb == 1)
-			save_param(env, j, nb_reg2, REG_CODE, 1);
-		if (nb_reg2 >= 1 && nb_reg2 <= 16)
-		{
-			reg_content = env->process[j].r[nb_reg2];
-			sum += reg_content;
-		}
-		cursor++;
-		nb_reg3 = env->process[j].op.saved[cursor];
-		if (env->verb == 1)
-			save_param(env, j, nb_reg3, REG_CODE, 2);
-		if (nb_reg1 >= 1 && nb_reg1 <= 16 && nb_reg2 >= 1 && nb_reg2 <= 16 && nb_reg3 >= 1 && nb_reg3 <= 16)
-		{
-			if (sum == 0)
-				env->process[j].carry = 1;
-			else
-				env->process[j].carry = 0;
-			env->process[j].op.name = "add";
-			env->process[j].r[nb_reg3] = sum;
-		}
-		cursor++;
+			save_param(env, j, nb_reg[2], REG_CODE, 2);
+		if (nb_reg[0] >= 1 && nb_reg[0] <= REG_NUMBER
+				&& nb_reg[1] >= 1 && nb_reg[1] <= REG_NUMBER
+				&& nb_reg[2] >= 1 && nb_reg[2] <= REG_NUMBER)
+			set_reg(env, j, sum, nb_reg[2]);
+		cursor += 4;
 	}
-else
+	else
 		cursor += decode_byte_param(env->process[j].op.saved[1], 0, 3);
 	env->process[j].pc += cursor;
 }
