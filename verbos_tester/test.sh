5b5e0d8063b4eaddad4e0a3bda298f9bbbfcee95
smakni <sabrimakni@gmail.com>
2019-06-28 17:33:17 +0200

    better verbos_tester

--- verbos_tester/test.sh
+++ verbos_tester/test.sh
@@ -12,100 +12,77 @@ OPT_2="-v 31 -a"
 echo -e "${LIGHTGREEN}***********************************************************"
 echo -e ">>>>>>>>>>>>>>>>>>>>VERBOS_TESTER<<<<<<<<<<<<<<<<<<<<<<<<<<"
 echo -e "***********************************************************${NOCOLOR}"
-read -p "Test a single champion ? (y/n) " answer
-if [ "$answer" = "y" ] || [ "$answer" = "Y" ] || [ "$answer" = "yes" ]
-then
-	unset options i
-	while IFS= read -r -d $'\0' f; do
-		options[i++]="$f"
-	done < <(find .. -name "*.cor" -print0 )
-	select champs in "${options[@]}" "quit"; do
-	case $champs in
+echo "Select the champions to test ?"
+unset options i
+while IFS= read -r -d $'\0' f; do
+	options[i++]="$f"
+done < <(find .. -name "*.cor" -print0 )
+let "i = 1"
+select champs in "${options[@]}" "stop selection"; do
+case $champs in
 	*.cor)
 		echo "$champs selected"
-		echo "Store first to exec_1.debug"
-		$PATH_1 $OPT_1 $champs > exec_1.debug
-		let "ret1 = $?"
-		if [ $ret1 != 0 ]
+		if [ $i = 1 ]
 		then
-			echo -e "${RED}ERROR : Path_1 not found${NOCOLOR}"
+			champ1="$champs"
+			((i++))
+		elif [ $i = 2 ]
+		then
+			champ2="$champs"
+			((i++))
+		elif [ $i = 3 ]
+		then
+			champ3="$champs"
+			((i++))
+		elif [ $i = 4 ]
+		then
+			champ4="$champs"
+			((i++))
 		else
-			echo -e "${LIGHTGREEN}[ok]${NOCOLOR}"
-		fi
-		echo "Store second to exec_2.debug"
-		$PATH_2 $OPT_2 $champs > exec_2.debug
-		let "ret2 = $?"
-		if [ $ret2 == 127 ]
-		then
-			echo -e "${RED}ERROR : Path_2 not found${NOCOLOR}"
-		else
-			echo -e "${LIGHTGREEN}[ok]${NOCOLOR}"
-		fi
-		if [ $ret1 != 0 ] || [ $ret2 == 127 ]
-		then
-			echo -e "${RED}ERROR : diff not possible${NOCOLOR}"
-			exit
-		else
-			read -p "Select arguments for diff : " args
-			read -p "Store the output ? | less ? (y/n/less) " answer2
-		fi
-		if [ "$answer2" = "y" ] || [ "$answer2" = "Y" ] || [ "$answer2" = "yes" ]
-		then
-			echo "Store diff to diff.debug"
-			diff $args exec_1.debug exec_2.debug > diff.debug
-		elif [ "$answer2" = "less" ]
-		then
-			diff $args exec_1.debug exec_2.debug | less
-		else
-			diff $args exec_1.debug exec_2.debug
+			echo "You already selected 4 champions(MAX)"
+			break
 		fi
 	;;
-	"quit")
-		exit
+	"stop selection")
+		break
 	;;
 	*)
 		echo "not listed"
 	;;
 	esac
 done
-fi
-find .. -name "*.cor"
-read -p "Write here the champions paths to test : " champs
-find .. $champs > /dev/null
-if [ $? != 0 ] || [ -z $champs ]
+
+if [ $i = 1 ] && [ -z $champ1]
 then
-	echo -e "${RED}ERROR : Wrong champions path${NOCOLOR}"
+	echo -e "${RED}No champions selected${NOCOLOR}"
+	exit
+fi
+
+echo "Store first to exec_1.debug"
+$PATH_1 $OPT_1 $champ1 $champ2 $champ3 $champ4 > exec_1.debug
+
+if [ $? != 0 ]
+then
+	echo -e "${RED}ERROR : Path_1 not found${NOCOLOR}"
 	exit
 else
-	echo "Store first to exec_1.debug"
-	$PATH_1 $OPT_1 $champs > exec_1.debug
-	find .. $PATH_1 > /dev/null
-	let "ret1 = $?"
-	if [ $ret1 != 0 ]
-	then
-		echo -e "${RED}ERROR : Path_1 not found${NOCOLOR}"
-	else
-		echo -e "${LIGHTGREEN}[ok]${NOCOLOR}"
-	fi
-	echo "Store second to exec_2.debug"
-	$PATH_2 $OPT_2 $champs > exec_2.debug
-	find .. $PATH_2 > /dev/null
-	let "ret2 = $?"
-	if [ $ret2 != 0 ]
-	then
-		echo -e "${RED}ERROR : Path_2 not found${NOCOLOR}"
-	else
-		echo -e "${LIGHTGREEN}[ok]${NOCOLOR}"
-	fi
-	if [ $ret1 != 0 ] || [ $ret2 == 127 ]
-	then
-		echo -e "${RED}ERROR : diff not possible${NOCOLOR}"
-		exit
-	else
-		read -p "Select arguments for diff : " args
-		read -p "Store the output ? | less ? (y/n/less) " answer2
-	fi
+	echo -e "${LIGHTGREEN}[ok]${NOCOLOR}"
 fi
+
+echo "Store second to exec_2.debug"
+$PATH_2 $OPT_2 $champ1 $champ2 $champ3 $champ4 > exec_2.debug
+
+if [ $? == 127 ]
+then
+	echo -e "${RED}ERROR : Path_2 not found${NOCOLOR}"
+	exit
+else
+	echo -e "${LIGHTGREEN}[ok]${NOCOLOR}"
+fi
+
+read -p "Select arguments for diff : " args
+read -p "Store the output ? | less ? (y/n/less) " answer2
+
 if [ "$answer2" = "y" ] || [ "$answer2" = "Y" ] || [ "$answer2" = "yes" ]
 then
 	echo "Store diff to diff.debug"
